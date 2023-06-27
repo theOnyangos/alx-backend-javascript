22b7c40aeaea09302cb97c4a14a31af29079abab
theOnynagos <otienodennis29@gmail.com>
2023-06-28 00:59:45 +0300

    Async await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+
+    return { photo, user };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
