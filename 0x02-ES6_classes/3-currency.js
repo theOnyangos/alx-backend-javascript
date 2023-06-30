b46eda8c0d518545937592f079af570fa420eeba
theOnynagos <otienodennis29@gmail.com>
2023-06-30 18:07:09 +0300

    Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,18 +3,23 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(n) {
     this._code = n;
   }
+
   get name() {
     return this._name;
   }
+
   set name(n) {
     this._name = n;
   }
+  
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
