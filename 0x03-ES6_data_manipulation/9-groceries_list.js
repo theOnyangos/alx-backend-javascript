1855a0ab06d123b3d6eda919e75d152998a79779
theOnynagos <otienodennis29@gmail.com>
2023-07-07 07:33:28 +0300

    Map list of Groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+// The function should return a map of the following items:
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
