8cad264f8f7b12ac14a91acade20cb3bed90260c
theOnynagos <otienodennis29@gmail.com>
2023-07-07 07:27:40 +0300

    Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+//  typed array
+export default function createInt8TypedArray(length, position, value) {
+  if (position > length - 1) throw Error('Position outside range');
+  const binary8Array = new Uint8Array(length);
+  binary8Array[position] = value;
+  return new DataView(binary8Array.buffer);
+}
